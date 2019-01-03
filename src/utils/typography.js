06e84e410372cb8890f3676693f3fd310bd67a4e
Julio <julio.olvr@gmail.com>
2019-01-02 22:05:50 -0300

    Typography.js

---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from 'typography';
+import theme from 'typography-theme-fairy-gates';
+
+const typography = new Typography({
+  ...theme,
+  baseFontSize: '20px',
+});
+
+export default typography;
