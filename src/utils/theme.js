0dee037dc993454659d4621ec588be8da76f48ec
Julio <julio.olvr@gmail.com>
2019-05-19 15:49:11 -0300

    Reuse text font from theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -26,12 +26,12 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     ${typography.body};
-    font-family: ${({ theme }) => theme.fonts.text}, serif;
+    font-family: ${({ theme }) => theme.fonts.serif};
     background-color: ${({ theme }) => theme.colors.background};
   }
 
   h1, h2, h3, h4, h5, h6 {
-    font-family: ${({ theme }) => theme.fonts.title}, sans-serif;
+    font-family: ${({ theme }) => theme.fonts.sansSerif};
   }
 
   h1 {
